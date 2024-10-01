b62392757d5f39fcea8e550b02ec1b87d6926339
Anvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-10-01 18:02:44 -0400

    Create alid_Palindrome.py

---
+++ alid_Palindrome.py
@@ -0,0 +1,9 @@
+# LINK to Question: https://leetcode.com/problems/valid-palindrome/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Convert characters to lowercase and remove non-alphanumeric characters
+        filteredString = ''.join(char.lower() for char in s if char.isalnum())
+
+        # Check if string is equal to it's reversed version
+        return filteredString == filteredString[::-1]
