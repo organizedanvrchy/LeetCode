57f8fc67a2a4e3ee8add76b3991ceee9842053c8
Anvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-10-07 21:11:45 -0400

    Update Longest_Substring_Without_Repeating_Characters.py

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -9,12 +9,12 @@ class Solution:
         if len(s) <= 1:
             return len(s)
         
-        for charIndex in range(len(s)):
-            if s[charIndex] in charIndexMap and charIndexMap[s[charIndex]] >= substringStart:
-                substringStart = charIndexMap[s[charIndex]] + 1
+        for substringEnd in range(len(s)):
+            if s[substringEnd] in charIndexMap and charIndexMap[s[substringEnd]] >= substringStart:
+                substringStart = charIndexMap[s[substringEnd]] + 1
             else:
-                substringMaxLength = max(substringMaxLength, charIndex - substringStart + 1)
+                substringMaxLength = max(substringMaxLength, substringEnd - substringStart + 1)
 
-            charIndexMap[s[charIndex]] = charIndex
+            charIndexMap[s[substringEnd]] = substringEnd
 
         return substringMaxLength
