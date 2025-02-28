3b4567720e8515f6f40a7b88f9cce7e7738967ae
Vimal <154699780+organizedanvrchy@users.noreply.github.com>
2025-02-28 10:53:36 -0500

    Update Add_Binary.py

--- Add_Binary.py
+++ Add_Binary.py
@@ -1,8 +1,13 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        # Using Built-In Python Functions
+        # res = bin(int(a, 2) + int(b, 2))[2:]
+        # return res
+
+        # 2nd Solution
+        
         # Initialize result as an empty list
         result = []
-        
         # Initialize carry to 0
         carry = 0
         
@@ -28,3 +33,44 @@ class Solution:
         
         # The result list is in reverse order, so reverse it and join to form a string
         return ''.join(reversed(result))
+
+
+        # 3rd solution
+        # class Solution:
+        #     def addBinary(self, a: str, b: str) -> str:
+        
+        #         result = []
+        #         carry = 0
+        
+        #         # Start from last character in both string
+        #         i = len(a) - 1
+        #         j = len(b) - 1
+        
+        #         while i >= 0 or j >= 0 or carry:
+        #             # Get bit from a or 0 if out of bounds
+        #             if i >= 0:
+        #                 bit_a = int(a[i])
+        #             else:
+        #                 bit_a = 0
+                    
+        #             # Get bit from b or 0 if out of bounds
+        #             if j >= 0:
+        #                 bit_b = int(b[j])
+        #             else:
+        #                 bit_b = 0
+                    
+        #             # Calculate the binary sum
+        #             binary_sum = bit_a + bit_b + carry
+        #             # Append last bit of the binary sum
+        #             result.append(str(binary_sum % 2))
+        #             # Update the carry
+        #             carry = binary_sum // 2
+        
+        #             # Move to left bit in string (if any)
+        #             i -= 1
+        #             j -= 1
+        
+        #         # Reverse and join list for resultant binary sum
+        #         output = ''.join(result[::-1])
+        #         return output
+        
